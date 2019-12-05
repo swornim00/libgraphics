5791fbf6dd3b15ec2a64c1e8a34827e66d98ecc7
swornim00 <swornimshrestha@Swornims-MacBook-Pro.local>
2019-12-05 18:30:00 +0545

    Initial Commit

---
+++ graphics.c
@@ -0,0 +1,61 @@
+#include"graphics.h"
+
+
+void initgraph(int *gdriver, int *gmode, char *pathtodriver){
+    window_h =500;
+    window_w =500;
+    if(gdriver != DETECT){
+        printf("Graphics Driver not found\n");
+    }
+    if(SDL_Init(SDL_INIT_EVERYTHING)<0){
+        printf("Failed to Initialize Video\n");
+    }
+
+    window = SDL_CreateWindow("Shivain Window",SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,window_h,window_w,SDL_WINDOW_RESIZABLE);
+    if(!window){
+        printf("Failed to create window");
+    } 
+
+    renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+}
+
+
+void checkq(){
+    SDL_RenderPresent(renderer);
+    int isrunning =1;
+    SDL_Event event;
+    while(isrunning){
+        while(SDL_PollEvent(&event)){
+            if(event.type ==SDL_QUIT){
+                isrunning =0;
+            }
+        }
+    }
+}
+    
+
+void closegraph(){    
+    checkq();
+    SDL_Quit();
+}
+
+void delay(int _mseconds){
+    clock_t starting_time = clock();
+    printf("Adding Delay");
+    printf("%lu\n",starting_time);
+    printf("%lu",starting_time+1000);
+    while(clock() < starting_time+_mseconds*100);
+}
+
+void putpixel(int x,int y, int _color){
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    SDL_RenderDrawPoint(renderer,x,y);
+}
+
+int getmaxx(){
+    return window_h;
+}
+
+int getmaxy(){
+    return window_w;
+}
---
+++ graphics.h
@@ -0,0 +1,23 @@
+#include<SDL2/SDL.h>
+
+/* graphic drivers */
+enum gdriver{DETECT=0, USER, VGA=9};
+//enum graphics_modes{VGALO=0, VGAMED, VGAHI, VGAMAX, VGA640, VGA800, VGA1024, USERMODE};
+
+ SDL_Renderer *renderer;
+ SDL_Window *window;
+/* As we have sixteen colors */
+enum _color{BLACK=0, BLUE, GREEN, CYAN, RED, MAGENTA, BROWN, LIGHTGRAY, DARKGRAY,LIGHTBLUE, LIGHTGREEN, LIGHTCYAN, LIGHTRED, LIGHTMAGENTA, YELLOW, WHITE};
+int window_h, window_w;
+void initgraph(int *gdriver, int *gmode, char *pathtodriver);
+void closegraph();
+void getch();
+void delay(int _mseconds);
+void checkq();
+void putpixel(int x, int y, int color);
+void line(int x1,int y1,int x2,int y2);
+
+int getmaxx();
+int getmaxy();
+int kbhit();
+
